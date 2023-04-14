4b01c20232711047299141c5db6d91d97a45d2d3
Muhammet Bozkurt <mubozkt@gmail.com>
2023-04-14 06:50:55 +0300

    Parsing
    parsing added

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -23,19 +23,28 @@ struct WeatherManager {
             // Step 2 : Create a URLSession
             let session = URLSession(configuration: .default)
             // Step 3 : Give the session a task
-            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                if let safeData = data {
+                    self.parseJSON(weatherData: safeData)
+                }
+            }
             // Step 4 : Start the task
             task.resume()
         }
     }
-    func handle(data: Data?, response: URLResponse?, error: Error?) {
-        if error != nil {
-            print(error!)
-            return
-        }
-        if let safeData = data {
-            let dataString = String(data: safeData, encoding: .utf8)
-            print(dataString)
+    
+    func parseJSON(weatherData: Data) {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            print(decodedData.weather[0].description)
+        } catch {
+            print(error)
         }
     }
+    
 }
